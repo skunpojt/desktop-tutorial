3174b6e655475fddd22666e489b632e0a5f64089
skunpojt <skunpojt@gmail.com>
2025-08-24 07:01:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+install('python-fasthtml')
+
 from fasthtml.common import *
 
 app,rt = fast_app()
